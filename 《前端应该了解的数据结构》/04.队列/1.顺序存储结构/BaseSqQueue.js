1da293b2cccd9268a080f0623eef034080e55f2a
SuperYing <13975145507@163.com>
2024-07-26 16:32:11 +0800

    feat: 顺序存储队列

---
+++ 《前端应该了解的数据结构》/04.队列/1.顺序存储结构/BaseSqQueue.js
@@ -0,0 +1,54 @@
+/**
+ * 基础队列
+ */
+class BaseSqQueue {
+
+  // 队列最大容量
+  MAX_SIZE = 10
+  // 元素数组
+  data = []
+  // 队列元素个数
+  length = 0
+  // 队尾指针
+  tail = -1
+
+  constructor(data) {
+    this.initQueue(data)
+  }
+
+  // 初始化队列结构
+  initQueue(data) {}
+
+  // 清空队列
+  clearQueue() {}
+
+  // 获取队头元素
+  getHead() {}
+
+  // 元素e入栈io'
+  enQueue(e) {
+    if (this.length >= this.MAX_SIZE) {
+      console.error('队列已满')
+      return
+    }
+    this.tail++
+    this.data[this.tail] = e
+    this.length++
+  }
+
+  // 队头元素出栈
+  deQueue() {
+    if (this.length === 0) {
+      console.warn('队列已空')
+      return null
+    }
+    const e = this.data[0]
+    for (let i = 0; i < this.data.length; i++) {
+      this.data[i] = this.data[i + 1]
+    }
+    this.data[this.tail] = null
+    this.tail--
+    this.length--
+    return e
+  }
+}
---
+++ 《前端应该了解的数据结构》/04.队列/Queue.js
@@ -0,0 +1,27 @@
+/**
+ * 队列
+ */
+class Queue {
+
+  // 队列元素个数
+  length = 0
+
+  constructor(data) {
+    this.initQueue(data)
+  }
+
+  // 初始化队列结构
+  initQueue(data) {}
+
+  // 清空队列
+  clearQueue() {}
+
+  // 获取队头元素
+  getHead() {}
+
+  // 元素e入栈
+  enQueue(e) {}
+
+  // 队头元素出栈
+  deQueue() {}
+}
